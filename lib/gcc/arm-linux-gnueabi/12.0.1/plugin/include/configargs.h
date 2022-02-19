85279227123a5c74b68421e91e1e81b193935fe6
ʏᴜᴜ <vcyzteen@protonmail.com>
2022-02-19 15:02:23 +0000

    Build Arm Gcc Nevixtoolchain
    Build completed on: Sat Feb 19 15:02:23 UTC 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-linux-gnueabi-gcc
    COLLECT_LTO_WRAPPER=/drone/gcc-arm/bin/../libexec/gcc/arm-linux-gnueabi/12.0.1/lto-wrapper
    Target: arm-linux-gnueabi
    Configured with: ../gcc/configure --target=arm-linux-gnueabi --prefix=/drone/src/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-docs --disable-werror --with-pkgversion=Nevixtoolchain --with-newlib --with-gnu-as --with-gnu-ld --enable-shared --enable-threads=posix --enable-__cxa_atexit --enable-clocale=gnu --enable-languages=all --disable-multilib --enable-linker-build-id --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib
    gcc version 12.0.1 20220219 (experimental) (Nevixtoolchain)
    
    Signed-off-by: ʏᴜᴜ <vcyzteen@protonmail.com>

---
+++ lib/gcc/arm-linux-gnueabi/12.0.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=arm-linux-gnueabi --prefix=/drone/src/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-docs --disable-werror --with-pkgversion=Nevixtoolchain --with-newlib --with-gnu-as --with-gnu-ld --enable-shared --enable-threads=posix --enable-__cxa_atexit --enable-clocale=gnu --enable-languages=all --disable-multilib --enable-linker-build-id --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
